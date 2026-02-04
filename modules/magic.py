1e54a867d47e132bdf53bc6936baf5a23234b549
mjennings754 <mitchellj@outlook.jp>
2026-02-05 08:53:55 +1100

    added to magic module

--- modules/magic.py
+++ modules/magic.py
@@ -4,3 +4,9 @@ def disappear(s):
         char if random.random() > 0.3 else " "
         for char in s
     )
+
+def magic_add(s):
+    return "".join(
+        char if random.random() > 0.5 else f"{random.randint(1, 100)}"
+        for char in s
+    )
