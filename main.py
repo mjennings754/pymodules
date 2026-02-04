cf385de8b7297a3c2775ebad02d1235841859f9c
mjennings754 <mitchellj@outlook.jp>
2026-02-04 20:11:14 +1100

    add reverse module

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from modules.arithmetic import *
-
+from modules.reverse import *
 print(add(5, 6))
 print(subtract(5, 6))
 print(multiply(5, 6))
 print(divide(5, 6))
 print(add_multiple(5, 5, 10))
+
+print(reverse_string('hello'))
