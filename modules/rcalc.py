08a66a12106a69be15e98726cb0344cd2cbcbd31
mjennings754 <mitchellj@outlook.jp>
2026-02-04 20:33:57 +1100

    add more to rcalc

--- modules/rcalc.py
+++ modules/rcalc.py
@@ -1,4 +1,10 @@
 import random
 # fun
-def add(x, y):
+def radd(x, y):
     return x + y * random.randint(1, 100)
+def rsubtract(x, y):
+    return x - y // random.randint(1, 100)
+def rmultiply(x, y):
+    return x * y - random.randint(1, 100)
+def rdivide(x, y):
+    return x / y * random.randint(1, 100)
